32f11f8f3ee6724fe3338180ccb659ba3cfc8b8e
Dat Nguyen <40033343+datnguyen199@users.noreply.github.com>
2019-12-29 11:33:14 +0700

    Singleton pattern (#6)

---
+++ src/com/datnguyen/designpatterns/singleton/Singleton2.java
@@ -0,0 +1,12 @@
+package com.datnguyen.designpatterns.singleton;
+
+public class Singleton2 {
+	private static Singleton2 uniqueInstance = new Singleton2();
+	
+	private Singleton2() {}
+	
+	public static Singleton2 getInstance() {
+		return uniqueInstance;
+	}
+
+}
